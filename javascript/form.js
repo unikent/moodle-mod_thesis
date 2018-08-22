720177e5e5fa3b9b9a4bf1679773dc1c6ad9598f
Dan Bartlett <d.bartlett@kent.ac.uk>
2018-08-22 10:42:20 +0100

    Fix third supervisor adding issue in form.js as fitem is created as class not id

--- javascript/form.js
+++ javascript/form.js
@@ -7,31 +7,30 @@ YUI().use('node', function(Y) {
 });
 
 var thesis_second_fields = '#id_second_supervisor_email, #id_second_supervisor_fname, #id_second_supervisor_sname';
-var thesis_second_divs = '#fitem_id_second_supervisor_email, #fitem_id_second_supervisor_fname, #fitem_id_second_supervisor_sname';
 var thesis_third_fields = '#id_third_supervisor_email, #id_third_supervisor_fname, #id_third_supervisor_sname';
-var thesis_third_divs = '#fitem_id_third_supervisor_email, #fitem_id_third_supervisor_fname, #fitem_id_third_supervisor_sname';
+var supervisor_button_id = '#id_more_supervisors';
 
 var thesis_hide_supervisors = function() {
 
   if (!Y.all(thesis_second_fields).get('value').join('')) {
-    Y.all(thesis_second_divs).setStyle('display','none');
+    $(thesis_second_fields).closest('.fitem').css('display', 'none');
   }
 
   if (!Y.all(thesis_third_fields).get('value').join('')) {
-    Y.all(thesis_third_divs).setStyle('display','none');
+    $(thesis_third_fields).closest('.fitem').css('display', 'none');
   } else {
-    Y.all(thesis_second_divs).setStyle('display','block');
-    Y.one('#id_more_supervisors').setStyle('display','none');
+    $(thesis_second_fields).closest('.fitem').css('display', 'block');
+    $(supervisor_button_id).css('display','none');
   }
 
 };
 
 var thesis_more_supervisors = function() {
 
-  if (Y.all(thesis_second_divs).getStyle('display').pop() == "none") {
-    Y.all(thesis_second_divs).setStyle('display','block');
-  } else if (Y.all(thesis_third_divs).getStyle('display').pop() == "none") {
-    Y.all(thesis_third_divs).setStyle('display','block');
-    Y.one('#id_more_supervisors').setStyle('display','none');
+  if ($(thesis_second_fields).closest('.fitem').css('display') == "none") {
+    $(thesis_second_fields).closest('.fitem').css('display', 'block');
+  } else if ( $(thesis_third_fields).closest('.fitem').css('display') == "none") {
+    $(thesis_third_fields).closest('.fitem').css('display', 'block');
+    $(supervisor_button_id).css('display','none');
   }
 };
